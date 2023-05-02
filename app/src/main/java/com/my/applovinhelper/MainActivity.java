2f8267d3b30bd82a02efc75af46d2449260736bd
Osama Projects <osama.projects1122@gmail.com>
2023-05-02 23:29:59 +0500

    Initial Commit

---
+++ app/src/main/java/com/my/applovinhelper/MainActivity.java
@@ -0,0 +1,16 @@
+package com.my.applovinhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.my.applovin_ads.Applovin_Ads;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
